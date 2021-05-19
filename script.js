d09757c340b5a8361c9a60f2457271b2495025f6
nacifyas <nacifyas@gmail.com>
2021-05-19 16:49:14 +0200

    Setting up js logic

---
+++ script.js
@@ -0,0 +1,20 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[hour-hand]');
+const minuteHand = document.querySelector('[minute-hand]');
+
+function setClock() {
+    const currentDate = new Date();
+    const secondRatio = currentDate.getSeconds() / 60;
+    const minuteRatio = (secondRatio + currentDate.getMinutes()) / 60;
+    const hourRatio = (minuteRatio + currentDate.getHours()) / 60; 
+
+    setRotation(minuteHand, minuteRatio)
+    setRotation(hourHand, hourRatio)
+}
+
+function setRotation(element, rotation) {
+
+}
+
+setClock();

1481770636384f6c28c672edec5ea9c4145d6354
nacifyas <nacifyas@gmail.com>
2021-05-19 19:00:27 +0200

    Added seconds hand

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ setInterval(setClock, 500)
 
 const hourHand = document.querySelector('[hour-hand]');
 const minuteHand = document.querySelector('[minute-hand]');
+const secondHand = document.querySelector('[second-hand]');
 
 function setClock() {
     const currentDate = new Date();
@@ -9,6 +10,7 @@ function setClock() {
     const minuteRatio = (secondRatio + currentDate.getMinutes()) / 60;
     const hourRatio = (minuteRatio + currentDate.getHours()) / 12; 
 
+    setRotation(secondHand, secondRatio)
     setRotation(minuteHand, minuteRatio);
     setRotation(hourHand, hourRatio);
 }
